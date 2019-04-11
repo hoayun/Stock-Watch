4206315bdf810961b3cdefcdf886144a5d2f75fe
Francisco <fcruz219@gmail.com>
2019-04-11 14:44:50 -0400

    made initial splash page. need to work on sign up and sign in page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,14 +6,12 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-
-  
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.render("signup");
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    res.render("index");
   });
 
   app.get("/login", function(req, res) {
